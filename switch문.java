01ec4b9dff04c9af545dd2a84b53f486791ebf0c
dongamma <101162785+dongamma@users.noreply.github.com>
2023-03-27 09:09:30 +0900

    Add files via upload

---
+++ switch문.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class switch문 {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      double a, c, result;
+      String b;
+      System.out.print("연산>>");
+      a = sc.nextDouble();
+      b = sc.next();
+      c = sc.nextDouble();
+
+      switch(b) {
+      case "+" :
+         result = a+c;
+         System.out.print(a+b+c+"의 계산 결과는 "+result);
+         break;
+      case "-":
+         result = a-c;
+         System.out.print(a+b+c+"의 계산 결과는 "+result);
+         break;
+      case "*":
+         result = a*c;
+         System.out.print(a+b+c+"의 계산 결과는 "+result);
+         break;
+      case "/":
+         if(c==0) {
+            System.out.print("0으로 나눌 수 없습니다.");
+            break;
+         }
+         else {
+            result = a / c;
+            System.out.print(a+b+c+"의 계산 결과는 "+result);
+            break;
+         }
+      default:
+         System.out.print("잘못 입력하였습니다.");
+      }
+      
+      sc.close();
+   }
+}
